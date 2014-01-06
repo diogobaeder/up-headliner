bd930d4dff9142548eca90f41d58ced6caae3f7a
Olivier Yiptong <olivier@olivieryiptong.com>
2014-01-06 16:36:22 +0400

    make utils load from json config

--- up/headliner/utils.py
+++ up/headliner/utils.py
@@ -6,7 +6,10 @@ from up.headliner import settings, DEFAULT_CONFIG_FILEPATH
 
 class SettingsObj(object):
     def __init__(self, **settings):
-        self.__dict__.update(settings)
+        self.update(**settings)
+
+    def update(self, **settings):
+        self.__dict__.update(**settings)
 
 def __read_config_file(options=None):
     # read default config
@@ -29,17 +32,7 @@ def __read_config_file(options=None):
             file_path = option.config
         with open(file_path, "r") as config_file:
             config = json.load(config_file)
-
-        if config.get('server') and config.get('storage'):
-            sys_settings = SettingsObj(**config)
-            config_obj.server['host'] = sys_settings.server.get('host', "127.0.0.1")
-            config_obj.server['port'] = sys_settings.server.get('port', 4355)
-            config_obj.server['debug'] = sys_settings.server.get('debug', False)
-            config_obj.redis['host'] = sys_settings.redis.get('host', '127.0.0.1')
-            config_obj.redis['port'] = sys_settings.redis.get('port', 6379)
-            config_obj.redis['database'] = sys_settings.redis.get('database', 0)
-            config_obj.redis['user'] = sys_settings.redis.get('user', None)
-            config_obj.redis['password'] = sys_settings.redis.get('password', None)
+            config_obj.update(**config)
 
     return config_obj
 
