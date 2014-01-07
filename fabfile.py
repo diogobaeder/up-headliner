9e0e082712913092d48cc8807f2c1c9ae2026a2a
Olivier Yiptong <olivier@olivieryiptong.com>
2014-01-07 11:40:51 +0400

    fabric deployment script

---
+++ fabfile.py
@@ -0,0 +1,58 @@
+import time
+from datetime import datetime
+from fabric.api import env, run, local, require, put
+
+env.path = "/var/www/headliner"
+env.user = "headliner"
+env.num_keep_releases = 10
+env.use_ssh_config = True
+env.release = time.strftime("%Y-%m-%d-%H-%M-%S", datetime.utcnow().timetuple())
+
+def upload_from_git():
+    """
+    Create tarball, send over the wire and untar
+    """
+    require("release", provided_by=[setup, deploy_cold, deploy])
+    local("git archive --format=tar master | bzip2 > %(release)s.tar.bz2" % env)
+    put("%(release)s.tar.bz2" % env, "/tmp/")
+
+    run("mkdir %(path)s/%(release)s" % env)
+    run("cd %(path)s/%(release)s && tar xjf /tmp/%(release)s.tar.bz2" % env)
+    run("rm /tmp/%(release)s.tar.bz2" % env)
+
+    local("rm %(release)s.tar.bz2" % env)
+
+def set_symlinks():
+    require("release", provided_by=[setup, deploy, deploy_cold])
+    run("rm %(path)s/previous" % env)
+    run("mv %(path)s/current %(path)s/previous" % env)
+    run("ln -s %(path)s/%(release)s %(path)s/current" % env)
+
+def setup_virtualenv():
+    require("release", provided_by=[setup, deploy, deploy_cold])
+    run("cd %(path)s/%(release)s && MOZ_UPHEADLINER_PROD=1 ./setup-project.sh" % env)
+
+def deploy_cold():
+    """
+    Deploy code but don"t change current running version
+    """
+    upload_from_git()
+    setup_virtualenv()
+
+def setup():
+    """
+    Create directories and deploy cold
+    """
+    run("mkdir -p /var/www/headliner")
+    deploy_cold()
+
+def deploy():
+    """
+    Deploy code, set symlinks and restart supervisor
+    """
+    deploy_cold()
+    set_symlinks()
+    restart_processes()
+
+def restart_processes():
+    run("sudo supervisorctl restart headliner:*")
