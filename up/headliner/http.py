b867e11cf97ae2f9c8708ae587961990f3a57fd3
Olivier Yiptong <olivier@olivieryiptong.com>
2014-01-28 16:28:02 +0400

    status api endpoint

--- up/headliner/http.py
+++ up/headliner/http.py
@@ -1,5 +1,10 @@
-from flask import Flask
+import up.headliner
+from flask import Flask, Response
 webapp = Flask("up.headliner")
+
+@webapp.route("/status")
+def root():
+    return Response("Headliner {0} STATUS OK".format(up.headliner.__VERSION__), mimetype="text/plain")
 
 def load_routes(routes):
     for route in routes:
