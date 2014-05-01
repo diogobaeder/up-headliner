4e023aec3fad11f2f73ffae97af383cb118ee06a
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2014-05-01 16:06:43 -0300

    Coloring fab task names to improve output

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,9 @@
 import time
 from datetime import datetime
 from distutils.util import strtobool
+from functools import wraps
 
+from fabric import colors
 from fabric.api import env, run, local, require, put
 
 env.path = "/var/www/headliner"
@@ -11,8 +13,18 @@ env.use_ssh_config = True
 env.release = time.strftime("%Y-%m-%d-%H-%M-%S", datetime.utcnow().timetuple())
 
 
+def section(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        print colors.cyan('Running section "{}"'.format(f.__name__), bold=True)
+        f(*args, **kwargs)
+        print
+    return wrapper
+
+
 ### Utility
 
+@section
 def upload_from_git():
     """
     Create tarball, send over the wire and untar
@@ -28,6 +40,7 @@ def upload_from_git():
     local("rm %(release)s.tar.bz2" % env)
 
 
+@section
 def set_symlinks():
     require("release", provided_by=[setup, deploy, deploy_cold])
     run("if [ -h %(path)s/previous ]; then rm %(path)s/previous; fi" % env)
@@ -35,11 +48,13 @@ def set_symlinks():
     run("ln -s %(path)s/%(release)s %(path)s/current" % env)
 
 
+@section
 def setup_virtualenv():
     require("release", provided_by=[setup, deploy, deploy_cold])
     run("cd %(path)s/%(release)s && MOZ_UPHEADLINER_PROD=1 ./setup-project.sh" % env)
 
 
+@section
 def clean_release_dir():
     """
     Delete releases if the number of releases to keep has gone beyond the threshold set by num_keep_releases
@@ -55,6 +70,7 @@ def clean_release_dir():
 
 ### Tasks
 
+@section
 def deploy_cold():
     """
     Deploy code but don"t change current running version
@@ -63,6 +79,7 @@ def deploy_cold():
     setup_virtualenv()
 
 
+@section
 def setup():
     """
     Create directories and deploy cold
@@ -71,6 +88,7 @@ def setup():
     deploy_cold()
 
 
+@section
 def deploy():
     """
     Deploy code, set symlinks and restart supervisor
@@ -81,6 +99,7 @@ def deploy():
     restart_processes()
 
 
+@section
 def restart_processes():
     run("sudo supervisorctl restart headliner:*")
 
@@ -100,6 +119,7 @@ def build():
     package()
 
 
+@section
 def test(config="nose.cfg", debug_errors=False, debug_failures=False):
     command = "nosetests --config={}".format(config)
     if to_bool(debug_errors):
@@ -109,10 +129,12 @@ def test(config="nose.cfg", debug_errors=False, debug_failures=False):
     local(command)
 
 
+@section
 def flake(config="flake8.cfg"):
     local("flake8 . --config={}".format(config))
 
 
+@section
 def package(clean=True):
     if to_bool(clean):
         local("rm -rf build/")
