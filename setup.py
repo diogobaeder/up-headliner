c5b3c5ae40ee167125b30ed875ecb98044a5482c
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2014-04-30 22:20:13 -0300

    Fixing setup.py for flake8

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 import os
 from setuptools import setup, find_packages
 
+
 requires = [
-        "Flask==0.10.1",
-        "celery==3.1.5",
-        "redis==2.8.0",
-        "hiredis==0.1.1",
-        "requests==2.0.1",
-        "furl==0.3.6",
-        "python-dateutil==2.2",
-        "Fabric==1.8.1",
-        "bleach==1.4",
+    "Flask==0.10.1",
+    "celery==3.1.5",
+    "redis==2.8.0",
+    "hiredis==0.1.1",
+    "requests==2.0.1",
+    "furl==0.3.6",
+    "python-dateutil==2.2",
+    "Fabric==1.8.1",
+    "bleach==1.4",
 ]
 
-if os.environ.has_key('MOZ_UPHEADLINER_DEV'):
+if 'MOZ_UPHEADLINER_DEV' in os.environ:
     requires.extend([
         "ipython==1.1.0",
         "nose==1.3.0",
@@ -21,20 +22,20 @@ if os.environ.has_key('MOZ_UPHEADLINER_DEV'):
         "mock==1.0.1",
         "flake8==2.1.0",
     ])
-if os.environ.has_key('MOZ_UPHEADLINER_PROD'):
+if 'MOZ_UPHEADLINER_PROD' in os.environ:
     requires.extend([
         "gunicorn==18.0",
         "gevent==1.0",
     ])
 
 setup(
-        name = "headliner",
-        version = "0.5",
-        description = "Demo content provider based on Mozilla UP interests",
-        author = "Mozilla",
-        packages=find_packages(),
-        package_data={"": ["*.lua"]},
-        include_package_data=True,
-        install_requires = requires,
-        scripts=["scripts/up-headliner-server", "scripts/up-headliner-scheduler", "scripts/up-headliner-worker", "scripts/populate_nytimes_mostpopular.py"],
+    name="headliner",
+    version="0.5",
+    description="Demo content provider based on Mozilla UP interests",
+    author="Mozilla",
+    packages=find_packages(),
+    package_data={"": ["*.lua"]},
+    include_package_data=True,
+    install_requires=requires,
+    scripts=["scripts/up-headliner-server", "scripts/up-headliner-scheduler", "scripts/up-headliner-worker", "scripts/populate_nytimes_mostpopular.py"],
 )
